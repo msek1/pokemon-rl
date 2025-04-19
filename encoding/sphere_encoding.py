4978877528446a25aaf7e45e874801caaf83b745
Misha Melnyk <as.k.github@melnyk.com>
2025-04-19 18:41:46 -0400

    fix re-making sphere encoding when importing

--- encoding/sphere_encoding.py
+++ encoding/sphere_encoding.py
@@ -9,32 +9,33 @@ def gen_point(N):
 ability_dim = 10
 item_dim = 10
 
-ability_encoding = {}
-item_encoding = {}
-constant_encodings = {"ability": ability_encoding, "item": item_encoding}
+if __name__ == "__main__":
+    ability_encoding = {}
+    item_encoding = {}
+    constant_encodings = {"ability": ability_encoding, "item": item_encoding}
 
-ability_encoding["UNKNOWN"] = torch.zeros(ability_dim)
-item_encoding["UNKNOWN"] = torch.zeros(item_dim)
-item_encoding["NONE"] = gen_point(item_dim)
+    ability_encoding["UNKNOWN"] = torch.zeros(ability_dim)
+    item_encoding["UNKNOWN"] = torch.zeros(item_dim)
+    item_encoding["NONE"] = gen_point(item_dim)
 
-with open("data/abilities.txt") as f:
-    abilities = map(lambda s: s.strip(), f.readlines())
-    for ab in abilities:
-        ability_encoding[ab] = gen_point(ability_dim)
+    with open("data/abilities.txt") as f:
+        abilities = map(lambda s: s.strip(), f.readlines())
+        for ab in abilities:
+            ability_encoding[ab] = gen_point(ability_dim)
 
-with open("data/items.txt") as f:
-    items = map(lambda s: s.strip(), f.readlines())
-    for item in items:
-        item_encoding[item] = gen_point(item_dim)
+    with open("data/items.txt") as f:
+        items = map(lambda s: s.strip(), f.readlines())
+        for item in items:
+            item_encoding[item] = gen_point(item_dim)
 
-with open("bot_data/constant_encodings.pkl", "wb") as f:
-    pickle.dump(constant_encodings, f)
+    with open("bot_data/constant_encodings.pkl", "wb") as f:
+        pickle.dump(constant_encodings, f)
 
-with open("bot_data/constant_encodings.pkl", "rb") as f:
-    ce2 = pickle.load(f)
+    with open("bot_data/constant_encodings.pkl", "rb") as f:
+        ce2 = pickle.load(f)
 
-assert(torch.all(ce2["ability"]["UNKNOWN"] == ability_encoding["UNKNOWN"]))
-assert(torch.all(ce2["ability"]["protean"] == ability_encoding["protean"]))
-assert(torch.all(ce2["item"]["UNKNOWN"] == item_encoding["UNKNOWN"]))
-assert(torch.all(ce2["item"]["NONE"] == item_encoding["NONE"]))
-assert(torch.all(ce2["item"]["zoomlens"] == item_encoding["zoomlens"]))
+    assert(torch.all(ce2["ability"]["UNKNOWN"] == ability_encoding["UNKNOWN"]))
+    assert(torch.all(ce2["ability"]["protean"] == ability_encoding["protean"]))
+    assert(torch.all(ce2["item"]["UNKNOWN"] == item_encoding["UNKNOWN"]))
+    assert(torch.all(ce2["item"]["NONE"] == item_encoding["NONE"]))
+    assert(torch.all(ce2["item"]["zoomlens"] == item_encoding["zoomlens"]))
