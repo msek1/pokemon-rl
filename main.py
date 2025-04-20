69fe36439f0f8fa2a74cf060939b99e0b5661053
Misha Melnyk <as.k.github@melnyk.com>
2025-04-20 18:45:28 -0400

    main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from poke_env import AccountConfiguration, ServerConfiguration
 from bot.bot import RLBot
 import bot.teams as teams
+from bot.bot_mapping import EnvironmentEncoder
 from encoding.autoencoder import Autoencoder
 from poke_env.player import RandomPlayer, SimpleHeuristicsPlayer
 import matplotlib.pyplot as plt
+import pickle
 
 from trainer.actorcritic import create_agent
 from trainer.trainer import Trainer
 import asyncio
+import torch
 import torch.optim as optim
 
 if __name__ == "__main__":
@@ -15,18 +18,16 @@ if __name__ == "__main__":
     NETWORK_DROPOUT = 0.2
     LEARNING_RATE = 0.001
     decision_network = create_agent(NETWORK_HIDDEN_DIM, NETWORK_DROPOUT)
-    
-    bot = RLBot("pokemonrlbotcs486", "gen7ou", "gen_7_team", decision_network)
-    
-    opp = SimpleHeuristicsPlayer(AccountConfiguration("rlbotopp", None), battle_format="gen7ou",  max_concurrent_battles=100, team=teams.teams["gen_7_team"])
-    
-    trainer = Trainer(battles_per_epoch=200)
 
+    torch.save(decision_network, "bot_data/main_model.pth")
+
+    trainer = Trainer(battles_per_epoch=50)
     optimizer = optim.Adam(decision_network.parameters(), lr=LEARNING_RATE)
-    # policy_loss, value_loss = trainer.run_epoch(bot, opp, optimizer)
-    # print(policy_loss, value_loss)
+    losses,evals = trainer.run_epochs(decision_network, optimizer, 10, checkpoint_interval=5)
 
-    losses,evals = trainer.run_epochs(bot, opp, optimizer, 10)
-
+    with open("training_results.pkl", "wb") as f:
+        pickle.dump({"loss": losses, "eval": evals}, f)
+    
+    print([l[0] for l in losses])
     plt.plot([l[0] for l in losses])
     plt.show()
