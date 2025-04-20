8af1a66a4adb7b2bdf126dccdfd5c1facff0b7c1
Matthew <matthew.sekirin@gmail.com>
2025-04-19 20:53:15 -0400

    actorcritic

---
+++ trainer/actorcritic.py
@@ -0,0 +1,46 @@
+import torch.nn as nn
+import torch.nn.functional as f
+
+from pokemon.bot.bot_mapping import STATE_DIMENSION
+
+class BackboneNetwork(nn.Module):
+    def __init__(self, in_features, hidden_dimensions, out_features, dropout):
+        super().__init__()
+        self.layer1 = nn.Linear(in_features, hidden_dimensions)
+        self.layer2 = nn.Linear(hidden_dimensions, hidden_dimensions)
+        self.layer3 = nn.Linear(hidden_dimensions, out_features)
+        self.dropout = nn.Dropout(dropout)
+    
+    def forward(self, x):
+        x = self.layer1(x)
+        x = f.relu(x)
+        x = self.dropout(x)
+        x = self.layer2(x)
+        x = f.relu(x)
+        x = self.dropout(x)
+        x = self.layer3(x)
+        return x
+
+class ActorCritic(nn.Module):
+    def __init__(self, actor, critic):
+        super().__init__()
+        self.actor = actor
+        self.critic = critic
+    def forward(self, state):
+        action_pred = self.actor(state)
+        value_pred = self.critic(state)
+        return action_pred, value_pred
+    
+def create_agent(hidden_dimensions, dropout):
+    ACTION_SPACE = 4 * 3 + 5
+    INPUT_FEATURES = STATE_DIMENSION 
+    HIDDEN_DIMENSIONS = hidden_dimensions
+    ACTOR_OUTPUT_FEATURES = ACTION_SPACE
+    CRITIC_OUTPUT_FEATURES = 1
+    DROPOUT = dropout
+    actor = BackboneNetwork(
+            INPUT_FEATURES, HIDDEN_DIMENSIONS, ACTOR_OUTPUT_FEATURES, DROPOUT)
+    critic = BackboneNetwork(
+            INPUT_FEATURES, HIDDEN_DIMENSIONS, CRITIC_OUTPUT_FEATURES, DROPOUT)
+    agent = ActorCritic(actor, critic)
+    return agent
